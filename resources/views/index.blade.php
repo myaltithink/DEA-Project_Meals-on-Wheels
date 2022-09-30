9a572b957a076ad102efcce9cb80a4aaccfe8a24
Arf <51786641+myaltithink@users.noreply.github.com>
2022-09-30 12:32:22 +0800

    favico done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="{{ URL::asset('favicon.ico') }}">
+</head>
+
+<body>
+
+    <p>test</p>
+
+</body>
+
+<script type="text/javascript" src="{!! HTML::script('js/test.js') !!}"></script>
+
+</html>
