606ab70ccb5e574e0c23ce88fc9013df0cb013e3
Arf <51786641+myaltithink@users.noreply.github.com>
2022-10-04 17:20:58 +0800

    role middle ware

---
+++ app/Http/Middleware/AuthorizeRole.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorizeRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $roles)
+    {
+        $count = 0;
+        foreach ($roles as $role) {
+            foreach (Auth::user()->roles as $user_roles) {
+                if (strtolower($role) == strtolower($user_roles->role_name)) {
+                    $count++;
+                }
+            }
+        }
+
+        if ($count == count($roles)) {
+            return $next($request);
+        } else {
+            return abort(401);
+        }
+    }
+}
