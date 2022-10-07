63b0e149fa50aa1d1c578fdd833c14118fc7bdb1
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2022-10-07 23:26:31 +0800

    Changes made for role based directive
    The directives encountered some bugs with the current logic to check for roles.
    
    Added permission check methods on user model.
    
    Added exclude role directive for rendering.
    
    Changed implementation for the following directives:
    
    -role directive
    -hasanyrole directive
    -hasroles directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,16 +31,8 @@ class AppServiceProvider extends ServiceProvider
         //the section to be rendered is only for specific roles pass argument as string
         Blade::directive('role', function ($expression) {
             return "<?php
-
-                    \$has_specified_role = false;
-                    foreach(Auth::user()->roles as \$role){
-                        if (strtolower(\$role->role_name) == strtolower($expression)){
-                            \$has_specified_role = true;
-                            break;
-                        }
-                    }
-
-                    if(\$has_specified_role){?>";
+                        if(Auth::user()->hasPermission($expression)){
+                    ?>";
         });
 
         Blade::directive('endrole', function () {
@@ -50,16 +42,8 @@ class AppServiceProvider extends ServiceProvider
         //use this if you require them to have multiple roles (all of them) pass argument as array
         Blade::directive('hasRoles', function ($expression) {
             return "<?php
-                    \$count = 0;
-                    foreach($expression as \$rolecheck){
-                        foreach(Auth::user()->roles as \$userrole){
-                            if(strtolower(\$rolecheck) == strtolower(\$userrole->role_name)){
-                                \$count++;
-                            }
-                        }
-                    }
-
-                    if(\$count == count($expression)){ ?>";
+                        if(Auth::user()->hasRoles($expression)){
+                    ?>";
         });
 
         Blade::directive('endHasRoles', function () {
@@ -69,21 +53,26 @@ class AppServiceProvider extends ServiceProvider
         //this is for has any roles, usage is @HasAnyRole(['roles']) in form of array
         Blade::directive('HasAnyRole', function ($expression) {
             return "<?php
-                    \$has_any_of_role = false;
-                    foreach($expression as \$rolecheck){
-                        foreach(Auth::user()->roles as \$userrole){
-                            if(strtolower(\$rolecheck) == strtolower(\$userrole->role_name)){
-                                \$has_any_of_role = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if(\$has_any_of_role){ ?>";
+                        if(Auth::user()->hasAnyRole($expression)){
+                    ?>";
         });
 
         Blade::directive('EndHasAnyRoles', function () {
             return "<?php } ?>";
         });
+
+        //render but excluding particular roles, better to use array of roles than string but string can also be used if only one role.
+        Blade::directive('ExcludeRole', function($expression){
+            return
+                "
+                <?php
+                    if(!Auth::user()->hasAnyRole($expression)){
+                ?>";
+        });
+
+        Blade::directive('EndExcludeRole', function(){
+            return "<?php } ?>";
+        });
+
     }
 }
--- resources/views/MealManagement/DeliveryManagement/meals-available.blade.php
+++ resources/views/MealManagement/DeliveryManagement/meals-available.blade.php
@@ -1,4 +1,27 @@
 @extends('MealManagement.DeliveryManagement.delivery-management-template')
 @section('main')
-    test
+    <div class = "row g-4">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12">
+            <div class="card" style="height:20rem;">
+                <div class = "card-header overflow-hidden p-0 bg-transparent" style="height: 10rem;">
+                    <img src = "{{asset('storage/foods/ayanokojik.jpg')}}" class="card-img-top"/>
+                </div>
+                <div class="card-body">
+                    <span class="text-center text-uppercase h3 d-block">WASDWASDWASDWAD</span>
+                </div>
+                <div class= "card-footer bg-transparent border border-0 p-2">
+                    @role('ROLE_MEMBER')
+                        <a href = "" class="btn btn-primary w-100">
+                            Order
+                        </a>
+                    @endrole
+                    @ExcludeRole('Role_member')
+                        <button class="btn btn-outline-success w-100" @disabled(true)>
+                            Approved Meal Plan
+                        </button>
+                    @EndExcludeRole
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
