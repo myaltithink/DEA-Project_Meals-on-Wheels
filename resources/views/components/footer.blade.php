a5b0086e7914a1948c17a265b7136d56fd6ce4b5
Arf <51786641+myaltithink@users.noreply.github.com>
2022-10-20 19:53:10 +0800

    a

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -21,7 +21,7 @@
             </a>
         </li>
         <li class="list-inline-item me-3 ms-3"><a href="/contact-us">Contact Us</a></li>
-        <li class="list-inline-item me-3 ms-3"><a href="#">About Us</a></li>
+        <li class="list-inline-item me-3 ms-3"><a href="/about-us">About Us</a></li>
         <li class="list-inline-item me-3 ms-3"><a href="/terms-and-condition">Terms And Condition</a></li>
     </ul>
     <p class="copyright">Copyright © Reserved for MerryMeals <br />Developed by Unity One Solutions</p>
