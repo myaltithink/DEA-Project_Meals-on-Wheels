11cf52f992434c8b9573b69d6494ee760a8badf6
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2022-10-28 15:10:47 +0800

    Added some metadata
    added metadata description for SEO

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="DEA MerryMeal Meals On Wheels">
     <title>{{ config('app.name', 'laravel') }}</title>
     <link rel="icon" href="{{ URL::asset('favicon.ico') }}">
     @vite(['resources/css/app.css', 'resources/css/app-mobile.css', 'resources/js/app.js',
@@ -18,7 +19,6 @@
 </head>
 
 <body>
-
     @include('components.header')
     @yield('content')
     @include('components.footer')
